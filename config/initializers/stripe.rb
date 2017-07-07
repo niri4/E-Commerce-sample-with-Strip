fa246a088eb7007094c1e3898a7e6ff725af31e1
niri4 <nirankarsingh247@gmail.com>
2017-07-07 14:51:13 +0530

    changes in stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,8 @@ Rails.configuration.stripe = {
 
   ### here we set test credential for the published key and secret key.
   ## In future this keys are store in rails environment variable because that not left to be opened
-  :publishable_key => 'pk_test_uUYBDqMLyHnkM1HXw7FIIsng',
-  :secret_key      => 'sk_test_Dnxy3CfcBMkBOs1ut73LqZIr'
+  :publishable_key => 'YOUR-STRIPE-PUBLISHABLE-KEY',
+  :secret_key      => 'YOUR-STRIPE-SECRET-KEY'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
